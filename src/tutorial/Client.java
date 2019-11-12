83dfaf8e079686fd90f0cf2554d4160ed6430054
Viktor Szendrei <szviktor10@gmail.com>
2019-11-12 19:55:23 +0000

    update files

--- src/tutorial/Client.java
+++ src/tutorial/Client.java
@@ -8,15 +8,40 @@ import javax.jms.*;
 
 
 public class Client {
-    public static void main(String[] args) throws JMSException, InterruptedException {
+    public static void main(String[] args) throws Exception {
+        EmbeddedBroker broker = new EmbeddedBroker();
+        broker.start();
 //        Create and start connection to broker
         String url = "tcp://localhost:61616";
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
         Connection connection = connectionFactory.createConnection();
         connection.start();
 
-//        Create a consumer, that receives and prints messages
-        Destination destination = new ActiveMQQueue("TUTORIAL.PRESENTER");
+        WriteTo(connection, "TUTORIAL.PRESENTER");
+
+        ReadFrom(connection, "TUTORIAL.PRESENTER");
+
+        connection.close();
+        broker.stop();
+    }
+
+    private static void ReadFrom(Connection connection, String name) throws JMSException {
+        Destination destination = new ActiveMQQueue(name);
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        MessageConsumer consumer = session.createConsumer(destination);
+        while(true) {
+            System.out.println("Waiting for message.");
+            ActiveMQTextMessage message = (ActiveMQTextMessage)consumer.receive(5000);
+            if (message == null) {
+                break;
+            }
+            System.out.println("Got message: " + message.getText());
+        }
+    }
+
+    private static void WriteTo(Connection connection, String name) throws JMSException, InterruptedException {
+        Destination destination = new ActiveMQQueue(name);
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
         MessageProducer producer = session.createProducer(destination);
@@ -26,17 +51,6 @@ public class Client {
             producer.send(message);
             Thread.sleep(500);
         }
-
-//        MessageConsumer consumer = session.createConsumer(destination);
-//        while(true) {
-//            System.out.println("Waiting for message.");
-//            ActiveMQTextMessage message = (ActiveMQTextMessage)consumer.receive(5000);
-//            if (message == null) {
-//                break;
-//            }
-//            System.out.println("Got message: " + message.getText());
-//        }
-
-        connection.close();
     }
+
 }
