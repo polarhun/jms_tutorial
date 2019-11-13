0c22c4cfb39bc31c513780c7ae75a0827b8dff30
polarhun <33174751+polarhun@users.noreply.github.com>
2019-11-13 11:44:10 +0000

    add user and password to stomp

--- src/stomp_tutorial.py
+++ src/stomp_tutorial.py
@@ -13,7 +13,7 @@ class MyListener(stomp.ConnectionListener):
 conn = stomp.Connection([("127.0.0.1", 61613)])
 conn.set_listener("", MyListener())
 conn.start()
-conn.connect(wait=True)
+conn.connect('user', 'password', wait=True)
 conn.subscribe(destination="/queue/TUTORIAL.PRESENTER", id=1, ack="auto")
 conn.send(body="HELLO FROM PYTHON", destination="/queue/TUTORIAL.PRESENTER")
 time.sleep(2)
